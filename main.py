9613dcac2750bb213ab93c09c2902dbdceb2237c
B-atuti <brian.atuti@student.moringaschool.com>
2023-09-07 18:32:53 +0300

    complete

--- main.py
+++ main.py
@@ -1,25 +1,25 @@
-# # main.py
-# from student_management.welcome import display_welcome_message
-# import click
+# main.py
+from student_management.welcome import display_welcome_message
+import click
 
-# @click.group(add_help_option=False)
-# def cli():
-#     pass
-
-
-# from student_management.student_registration import add_student, delete_student
-# from student_management.course_management import create_course, delete_course
-# from student_management.enrollment import enroll_student
-# from student_management.basic_reporting import student_report
+@click.group(add_help_option=False)
+def cli():
+    pass
 
 
-# cli.add_command(add_student)
-# cli.add_command(create_course)
-# cli.add_command(enroll_student)
-# cli.add_command(student_report)
-# cli.add_command(delete_student)
-# cli.add_command(delete_course)
+from student_management.student_registration import add_student, delete_student
+from student_management.course_management import create_course, delete_course
+from student_management.enrollment import enroll_student
+from student_management.basic_reporting import student_report
 
-# if __name__ == '__main__':
-#     display_welcome_message()  
-#     cli()
+
+cli.add_command(add_student)
+cli.add_command(create_course)
+cli.add_command(enroll_student)
+cli.add_command(student_report)
+cli.add_command(delete_student)
+cli.add_command(delete_course)
+
+if __name__ == '__main__':
+    display_welcome_message()  
+    cli()
