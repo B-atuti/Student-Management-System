9af1db0f0750b92a5f6818a6712009808cddcdb7
B-atuti <brian.atuti@student.moringaschool.com>
2023-09-07 18:28:52 +0300

    complete

--- student_management/database.py
+++ student_management/database.py
@@ -1,6 +1,6 @@
-# from sqlalchemy import create_engine
-# from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
-# DATABASE_URI = "sqlite:///student_management.db"
-# engine = create_engine(DATABASE_URI)
-# Session = sessionmaker(bind=engine)
+DATABASE_URI = "sqlite:///student_management.db"
+engine = create_engine(DATABASE_URI)
+Session = sessionmaker(bind=engine)
