3f0bbb9f3c35ceb2b86c6661363da1a6aacec571
B-atuti <brian.atuti@student.moringaschool.com>
2023-09-07 18:56:33 +0300

    pytests

--- tests/test_database.py
+++ tests/test_database.py
@@ -1,17 +1,17 @@
-# import unittest
-# from student_management.database import Session
-# from student_management.models import Student
+import unittest
+from student_management.database import Session
+from student_management.models import Student
 
-# class TestDatabaseOperations(unittest.TestCase):
-#     def test_insert_student(self):
-#         session = Session()
-#         student = Student(name="John Doe", contact_info="john@example.com")
-#         session.add(student)
-#         session.commit()
+class TestDatabaseOperations(unittest.TestCase):
+    def test_insert_student(self):
+        session = Session()
+        student = Student(name="John Doe", contact_info="john@example.com")
+        session.add(student)
+        session.commit()
 
-#         # Check if the student was inserted correctly
-#         retrieved_student = session.query(Student).filter_by(name="John Doe").first()
-#         self.assertIsNotNone(retrieved_student)
-#         self.assertEqual(retrieved_student.contact_info, "john@example.com")
+        # Check if the student was inserted correctly
+        retrieved_student = session.query(Student).filter_by(name="John Doe").first()
+        self.assertIsNotNone(retrieved_student)
+        self.assertEqual(retrieved_student.contact_info, "john@example.com")
 
-#         session.close()
+        session.close()
--- tests/test_student_management.py
+++ tests/test_student_management.py
@@ -1,14 +1,14 @@
-# import unittest
-# from click.testing import CliRunner
-# from student_management.cli import add_student
+import unittest
+from click.testing import CliRunner
+from student_management.student_registration import add_student
 
-# class TestCLIStudentManagement(unittest.TestCase):
-#     def setUp(self):
-#         self.runner = CliRunner()
+class TestCLIStudentManagement(unittest.TestCase):
+    def setUp(self):
+        self.runner = CliRunner()
 
-#     def test_add_student_command(self):
-#         result = self.runner.invoke(add_student, ["--name", "Alice", "--contact_info", "alice@example.com"])
-#         self.assertEqual(result.exit_code, 0)  # Check if the command exits successfully
+    def test_add_student_command(self):
+        result = self.runner.invoke(add_student, ["--name", "Alice", "--contact_info", "alice@example.com"])
+        self.assertEqual(result.exit_code, 0)  # Check if the command exits successfully
 
-#         # Check if the command output contains a success message
-#         self.assertIn("Student Alice added successfully.", result.output)
+        # Check if the command output contains a success message
+        self.assertIn("Student Alice added successfully.", result.output)
