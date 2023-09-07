5fea91ed84d3468cfc29557a208b5704897d4776
B-atuti <brian.atuti@student.moringaschool.com>
2023-09-07 18:30:22 +0300

    complete

--- student_management/models.py
+++ student_management/models.py
@@ -1,49 +1,49 @@
-# from sqlalchemy import Column, Integer, String, ForeignKey, create_engine, Table
-# from sqlalchemy.orm import relationship, sessionmaker
-# from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String, ForeignKey, create_engine, Table
+from sqlalchemy.orm import relationship, sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
 
-# Base = declarative_base()
+Base = declarative_base()
 
-# enrollment_association = Table(
-#     'enrollments',
-#     Base.metadata,
-#     Column('student_id', Integer, ForeignKey('students.id'), primary_key=True),
-#     Column('course_id', Integer, ForeignKey('courses.id'), primary_key=True)
-# )
+enrollment_association = Table(
+    'enrollments',
+    Base.metadata,
+    Column('student_id', Integer, ForeignKey('students.id'), primary_key=True),
+    Column('course_id', Integer, ForeignKey('courses.id'), primary_key=True)
+)
 
-# class Student(Base):
-#     __tablename__ = 'students'
+class Student(Base):
+    __tablename__ = 'students'
 
-#     id = Column(Integer, primary_key=True)
-#     name = Column(String(100), nullable=False)
-#     contact_info = Column(String(100))
+    id = Column(Integer, primary_key=True)
+    name = Column(String(100), nullable=False)
+    contact_info = Column(String(100))
 
 
-#     courses = relationship('Course', secondary=enrollment_association, back_populates='students')
+    courses = relationship('Course', secondary=enrollment_association, back_populates='students')
 
-#     def __repr__(self):
-#         return f"<Student(id={self.id}, name='{self.name}', contact_info='{self.contact_info}')>"
+    def __repr__(self):
+        return f"<Student(id={self.id}, name='{self.name}', contact_info='{self.contact_info}')>"
     
-#     def delete(self, session):
-#         session.delete(self)
+    def delete(self, session):
+        session.delete(self)
 
-# class Course(Base):
-#     __tablename__ = 'courses'
+class Course(Base):
+    __tablename__ = 'courses'
 
-#     id = Column(Integer, primary_key=True)
-#     name = Column(String(100), nullable=False)
+    id = Column(Integer, primary_key=True)
+    name = Column(String(100), nullable=False)
 
-#     students = relationship('Student', secondary=enrollment_association, back_populates='courses')
+    students = relationship('Student', secondary=enrollment_association, back_populates='courses')
 
-#     def __repr__(self):
-#         return f"<Course(id={self.id}, name='{self.name}')>"
+    def __repr__(self):
+        return f"<Course(id={self.id}, name='{self.name}')>"
     
-#     def delete(self, session):
-#         session.delete(self)
+    def delete(self, session):
+        session.delete(self)
 
-# DATABASE_URI = "sqlite:///student_management.db"
-# engine = create_engine(DATABASE_URI)
+DATABASE_URI = "sqlite:///student_management.db"
+engine = create_engine(DATABASE_URI)
 
-# Base.metadata.create_all(engine)
+Base.metadata.create_all(engine)
 
-# Session = sessionmaker(bind=engine)
+Session = sessionmaker(bind=engine)
