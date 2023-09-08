8c243c621a766819cf21ef4738e3c0d9b336fc56
B-atuti <brian.atuti@student.moringaschool.com>
2023-09-08 15:33:29 +0300

    done

--- student_management/database.py
+++ student_management/database.py
@@ -1,6 +1,14 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import relationship, sessionmaker, declarative_base
+
+Base = declarative_base()
+
 
 DATABASE_URI = "sqlite:///student_management.db"
 engine = create_engine(DATABASE_URI)
 Session = sessionmaker(bind=engine)
+
+
+
+Base.metadata.create_all(engine)
